58a17b1518f4d381ddfb52dbaf187de8d7cfb599
SauravSASTRA <sippsaurav24@gmail.com>
2018-08-22 14:27:47 +0530

    Sherlock and Anagrams

---
+++ anagram.cpp
@@ -0,0 +1,4 @@
+/* Anagram
+An anagram is a permutation only applicable to strings. It's a re-arrangement of the characters.
+Two strings are called an anagrammatic pair when they have the same length and the same set of characters.
+*/
