bb5adfa3d61c343487009713769aa8c299aa4d5f
SauravSASTRA <sippsaurav24@gmail.com>
2018-08-29 10:11:04 +0530

    Create Lower_Bound_STL.cpp
    https://www.hackerrank.com/challenges/cpp-lower-bound/problem

---
+++ Lower_Bound_STL.cpp
@@ -0,0 +1,61 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <math.h>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int size=0, querysize=0, sdigit=0, temp=0;
+    vector<int> varray;
+    cin>>size;
+    //cin.ignore(numeric_limits<streamsize>::max(),'\n');
+    for(int i=0; i< size; i++)
+    {
+        cin>>temp;
+        varray.push_back(temp);
+    }
+    
+    cin>>querysize;
+    
+    for(int i=1; i<=querysize; i++)
+    {
+        int found=0;
+        //vector<int> diff;
+        cin>>sdigit;
+        for(int j=0; j < varray.size(); j++)
+        {
+            if(sdigit == varray[j])
+            {
+                cout<<"Yes "<<j+1<<endl;
+                found = 1;
+                break;
+            }
+        }
+        if(!found)
+        {
+            vector<int> diff;
+            for(int j=0; j < varray.size(); j++)
+            {
+               diff.push_back(abs(varray[j]-sdigit));
+            }
+
+            //sort(diff.begin(), diff.end());
+            int sindex=0;
+            //3 3 2 2 2 5 5 11 
+            for(int j=1; j<diff.size(); j++)
+            {
+                //cout<<"compare"<<diff[sindex]<<"and"<<diff[j]<<endl;
+                if(diff[sindex]>=diff[j])
+                    sindex = j;
+                //cout<<"sindex = "<<sindex<<endl;
+            }
+            //cout<<endl<<"sindex= "<<sindex<<endl;
+            cout<<"No "<<sindex+1<<endl;
+        }
+    }
+    return 0;
+}
